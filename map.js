f030cd1b5069cbca2db7e853953de3f20698092f
Arjun <junnaonly@gmail.com>
2015-09-16 23:19:51 +0300

    added polyline animation

--- map.js
+++ map.js
@@ -1,4 +1,8 @@
-var map = L.map('map').setView([60.190, 24.832], 13);
+var otaniemi_xy = [60.190, 24.832];
+var tapiola_xy = [60.176, 24.808];
+var lauttasaari_xy = [60.157, 24.874];
+
+var map = L.map('map').setView(otaniemi_xy, 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -12,3 +16,45 @@ var dump2 = L.marker([60.190, 24.837]).addTo(map);
 
 dump1.bindPopup("Dump 1").openPopup();
 dump2.bindPopup("Dump 2");
+
+function showTapiola(){
+  map.setView(tapiola_xy, 13);
+}
+
+function showLauttasaari(){
+  map.setView(lauttasaari_xy, 13);
+}
+
+function animate_line(){
+
+  var latLangs2 = [];
+  latLangs2.push(otaniemi_xy);
+  latLangs2.push(tapiola_xy);
+  latLangs2.push(lauttasaari_xy);
+
+  //var polyline2 = L.polyline(latLangs2, {color: 'green'}).addTo(map);
+
+  var polyline = L.polyline([], {color: 'red'}).addTo(map);
+  
+  setTimeout(function(){
+    polyline.addLatLng(otaniemi_xy);
+  }, 0);
+  
+  setTimeout(function(){
+  polyline.addLatLng(tapiola_xy);
+  }, 0);
+  
+  setTimeout(function(){
+  polyline.addLatLng(lauttasaari_xy);
+  }, 3000);
+  
+  setTimeout(function(){
+  polyline.addLatLng(otaniemi_xy);
+  }, 6000);
+}
+
+
+function showOtaniemi(){
+  map.setView(otaniemi_xy, 12);
+  animate_line();
+}
