8c4bc574fbb410e642d4337399cc71709c0fd630
Arjun <junnaonly@gmail.com>
2015-09-27 02:01:44 +0300

    going to sleep

--- indiAread3.js
+++ indiAread3.js
@@ -12,6 +12,11 @@ var chart = c3.generate({
             Mixed: 'area-spline'
             // 'line', 'spline', 'step', 'area', 'area-step' are also available to stack
         },
+        colors: {
+           Bio: '#78BE20',
+	    Mixed: '#009CA6',
+	    Energy: '#AF272F'
+        },
         groups: [['Bio', 'Energy', 'Mixed']]
     },
     zoom: {
@@ -23,23 +28,30 @@ var chart = c3.generate({
 });
 
 
-var chart2 = c3.generate({
- bindto: '#compareChartBio',
+var chart7 = c3.generate({
+  bindto: '#basicNeighbour',
     data: {
         columns: [
-            ['Me', 128,153,109,183,112,103,157,179,163,163,163,144,198,172,191,138,175,118,131,198,173,116],
-            ['Best', 613,691,504,522,659,650,530,658,594,549,655,518,613,575,667,674,674,679,623,542,596,699],
-            ['Worst', 1384,1366,1458,1485,1460,1459,1452,1574,1457,1496,1235,1354,1458,1368,1156,1524,1245,1554,1211,1454,1453,1560]
+            [' ', 30, 40, 50]
         ],
-        type: 'bar'
+        type: 'bar',
+        color: function (color, d) {
+            // d will be 'id' when called for legends
+            return d.index === 2? "#78BE20": d.index === 1? "#009CA6": "#AF272F";
+        }
     },
     axis: {
-      x: {
-         extent: [5, 4.5]
-      }
+        rotated: true,
+        x: {
+            type: 'category',
+            categories: ['BEST', 'YOU', 'AVERAGE'],
+	    tick: {
+                fit: true
+            }
+        }
     },
-    zoom: {
-        enabled: true
+    legend : {
+        hide: true
     }
 });
 
@@ -47,26 +59,16 @@ var chart3 = c3.generate({
  bindto: '#compareChartEnergy',
     data: {
         columns: [
-            ['Me', 128,153,109,183,112,103,157,179,163,163,163,144,198,172,191,138,175,118,131,198,173,116],
-            ['Best', 613,691,504,522,659,650,530,658,594,549,655,518,613,575,667,674,674,679,623,542,596,699],
-            ['Worst', 1384,1366,1458,1485,1460,1459,1452,1574,1457,1496,1235,1354,1458,1368,1156,1524,1245,1554,1211,1454,1453,1560]
+            ['Best', 128,153,109,183,112,103,157,179,163,163,163,144,198,172,191,138,175,118,131,198,173,116],
+            ['You', 613,691,504,522,659,650,530,658,594,549,655,518,613,575,667,674,674,679,623,542,596,699],
+            ['Average', 1384,1366,1458,1485,1460,1459,1452,1574,1457,1496,1235,1354,1458,1368,1156,1524,1245,1554,1211,1454,1453,1560]
         ],
-        type: 'bar'
-    },
-    zoom: {
-        enabled: true
-    }
-});
-
-var chart4 = c3.generate({
- bindto: '#compareChartMixed',
-    data: {
-        columns: [
-            ['Me', 128,153,109,183,112,103,157,179,163,163,163,144,198,172,191,138,175,118,131,198,173,116],
-            ['Best', 613,691,504,522,659,650,530,658,594,549,655,518,613,575,667,674,674,679,623,542,596,699],
-            ['Worst', 1384,1366,1458,1485,1460,1459,1452,1574,1457,1496,1235,1354,1458,1368,1156,1524,1245,1554,1211,1454,1453,1560]
-        ],
-        type: 'bar'
+        type: 'bar',
+        colors: {
+            Best: '#AF272F',
+	    You: '#009CA6',
+	    Average: '#78BE20'
+        }
     },
     zoom: {
         enabled: true
@@ -77,7 +79,7 @@ var chart5 = c3.generate({
   bindto: '#userRating',
     data: {
         columns: [
-            ['Are you better than you?', 30, 40, 50]
+            [' ', 30, 40, 50]
         ],
         type: 'bar',
         color: function (color, d) {
@@ -89,8 +91,14 @@ var chart5 = c3.generate({
         rotated: true,
         x: {
             type: 'category',
-            categories: ['Your best', 'YOU', 'Your Worst']
+            categories: ['BEST', 'NOW', 'WORST'],
+	    tick: {
+                fit: true
+            }
         }
+    },
+    legend : {
+        hide: true
     }
 });
 
@@ -101,6 +109,11 @@ var chart6 = c3.generate({
             ['Bio', 10],
             ['Mixed', 60],
             ['Energy',30]
-        ], type: "donut"
+        ], type: "donut",
+        colors: {
+            Bio: '#78BE20',
+	    Mixed: '#009CA6',
+	    Energy: '#AF272F'
+        }
     }
 });
